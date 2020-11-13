7e4757f0f3f5be61374e3e1730c9d961406c3aa1
MikelDela <mikel.delatorre@opendeusto.es>
2020-11-13 10:34:05 +0100

    Clases Usuario y Vuelo

--- VuelingProyect/src/clases/Usuario.java
+++ VuelingProyect/src/clases/Usuario.java
@@ -5,4 +5,58 @@ public class Usuario {
 	private String nombre;
 	private String apellido;
 	private String email;
+	
+	public Usuario(String username, String nombre, String apellido, String email) {
+		this.username = username;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+	}
+	
+	public Usuario() {
+		this.username = "";
+		this.nombre = "";
+		this.apellido = "";
+		this.email = "";
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [username=" + username + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
+				+ "]";
+	}
+	
+	
 }
--- VuelingProyect/src/clases/Vuelo.java
+++ VuelingProyect/src/clases/Vuelo.java
@@ -8,4 +8,69 @@ public class Vuelo {
 	private double precio;
 	private Date fecha_Salida;
 	private Date fecha_Llegada;
+	
+	public Vuelo(int id_Vuelo, int plazas_libres, double precio, Date fecha_Salida, Date fecha_Llegada) {
+		this.id_Vuelo = id_Vuelo;
+		this.plazas_libres = plazas_libres;
+		this.precio = precio;
+		this.fecha_Salida = fecha_Salida;
+		this.fecha_Llegada = fecha_Llegada;
+	}
+	
+	public Vuelo() {
+		this.id_Vuelo = 0;
+		this.plazas_libres = 0;
+		this.precio = 0.0;
+		this.fecha_Salida = null;
+		this.fecha_Llegada = null;
+	}
+
+	public int getId_Vuelo() {
+		return id_Vuelo;
+	}
+
+	public void setId_Vuelo(int id_Vuelo) {
+		this.id_Vuelo = id_Vuelo;
+	}
+
+	public int getPlazas_libres() {
+		return plazas_libres;
+	}
+
+	public void setPlazas_libres(int plazas_libres) {
+		this.plazas_libres = plazas_libres;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public Date getFecha_Salida() {
+		return fecha_Salida;
+	}
+
+	public void setFecha_Salida(Date fecha_Salida) {
+		this.fecha_Salida = fecha_Salida;
+	}
+
+	public Date getFecha_Llegada() {
+		return fecha_Llegada;
+	}
+
+	public void setFecha_Llegada(Date fecha_Llegada) {
+		this.fecha_Llegada = fecha_Llegada;
+	}
+
+	@Override
+	public String toString() {
+		return "Vuelo [id_Vuelo=" + id_Vuelo + ", plazas_libres=" + plazas_libres + ", precio=" + precio
+				+ ", fecha_Salida=" + fecha_Salida + ", fecha_Llegada=" + fecha_Llegada + "]";
+	}
+
+	
+	
 }
