5c83516f3d1cffaa4e7e6e3036ea975438221691
mikelgomez <mikelgomez@opendeusto.es>
2020-11-13 10:30:56 +0100

    Constructor, Getter y Setter de Aerolinea y Aeropuerto

--- VuelingProyect/src/clases/Aerolinea.java
+++ VuelingProyect/src/clases/Aerolinea.java
@@ -3,4 +3,38 @@ package clases;
 public class Aerolinea {
 	private int id;
 	private String nombre;
+	
+	public Aerolinea(int id, String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	public Aerolinea() {
+		this.id = 0;
+		this.nombre = "";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Aerolinea [id=" + id + ", nombre=" + nombre + "]";
+	}
+	
+	
+	
 }
--- VuelingProyect/src/clases/Aeropuerto.java
+++ VuelingProyect/src/clases/Aeropuerto.java
@@ -3,4 +3,34 @@ package clases;
 public class Aeropuerto {
 	private int id_Aeropuerto;
 	private String nombre;
+	
+	public Aeropuerto(int id_Aeropuerto, String nombre) {
+		super();
+		this.id_Aeropuerto = id_Aeropuerto;
+		this.nombre = nombre;
+	}
+
+	public int getId_Aeropuerto() {
+		return id_Aeropuerto;
+	}
+
+	public void setId_Aeropuerto(int id_Aeropuerto) {
+		this.id_Aeropuerto = id_Aeropuerto;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Aeropuerto [id_Aeropuerto=" + id_Aeropuerto + ", nombre=" + nombre + "]";
+	}
+	
+	
+	
 }
